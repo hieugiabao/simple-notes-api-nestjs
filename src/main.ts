b09b7031724d31441ff3f4a2828c7dfebad47d43
hieugiabao <hieugiabao12@gmail.com>
2022-09-09 08:55:26 +0700

    add swagger doc ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { createDocument } from './swagger/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  SwaggerModule.setup('api/v1', app, createDocument(app));
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
