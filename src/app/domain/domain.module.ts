07b600fbbd233269efebc921c54fa6956d4c5021
hieu <hieugiabao12@gmail.com>
2022-09-13 16:07:51 +0700

    add ci

--- src/app/domain/domain.module.ts
+++ src/app/domain/domain.module.ts
@@ -9,7 +9,11 @@ import { NoteController } from './controllers/note.controller';
 
 export const ALL_ENTITIES = fs
   .readdirSync(path.join(path.dirname(__filename), 'entities'))
-  .filter((file) => path.extname(file) === '.js')
+  .filter(
+    (file) =>
+      path.extname(file) === '.js' ||
+      (path.extname(file) === '.ts' && !file.endsWith('.d.ts')),
+  )
   .map((file) => require(`./entities/${file}`).default as Type<any>);
 
 export const ALL_SERVICES = fs
